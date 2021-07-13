19fc69bdb61feea96317e5f7d0f2023e4528af84
roburi <rvbr.rvbr@gmail.com>
2021-07-13 13:02:06 -0600

    interface CRUD

--- certificacionProsenior/src/main/java/Interfaces/CRUD.java
+++ certificacionProsenior/src/main/java/Interfaces/CRUD.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Interfaces;
 
-/**
- *
- * @author Usuario
- */
-public class CRUD {
-    
+import Modelo.Empleado;
+import java.util.List;
+
+public interface CRUD {
+    public List listar();
+    public Empleado list(int num_empleado);
+    public boolean nuevo(Empleado emp);
+    public boolean editar(Empleado emp);
+    public boolean eliminar(Empleado emp);
 }
